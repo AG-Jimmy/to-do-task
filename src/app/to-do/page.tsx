1b37190e004b896de1288f5454fb2ca07e408319
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2025-08-31 15:37:08 +0300

    feat: implement data fetching for Kanban columns using React Query

--- src/app/to-do/page.tsx
+++ src/app/to-do/page.tsx
@@ -1,13 +1,70 @@
 "use client";
+import { useQueries, UseQueryResult } from "@tanstack/react-query";
+import {
+  fetchBacklogTasks,
+  fetchDoneTasks,
+  fetchInProgressTasks,
+  fetchReviewTasks,
+} from "./api/toDoApi";
 import { KanbanColumn, SearchBar } from "./components";
 
+type Q<T> = UseQueryResult<T[], unknown>;
+
 const HomePage = () => {
-  const columns: ColumnProps[] = [
+  const columns = [
     { id: "backlog", title: "Backlog" },
     { id: "in-progress", title: "In Progress" },
     { id: "review", title: "Review" },
     { id: "done", title: "Done" },
   ];
+
+  const [doneQ, backlogQ, inProgressQ, reviewQ] = useQueries({
+    queries: [
+      {
+        queryKey: ["done", 1, 5],
+        queryFn: () => fetchDoneTasks({ page: 1, limit: 5 }),
+        staleTime: 10_000,
+      },
+      {
+        queryKey: ["backlog", 1, 5],
+        queryFn: () => fetchBacklogTasks({ page: 1, limit: 5 }),
+        staleTime: 10_000,
+      },
+      {
+        queryKey: ["in-progress", 1, 5],
+        queryFn: () => fetchInProgressTasks({ page: 1, limit: 5 }),
+        staleTime: 10_000,
+      },
+      {
+        queryKey: ["review", 1, 5],
+        queryFn: () => fetchReviewTasks({ page: 1, limit: 5 }),
+        staleTime: 10_000,
+      },
+    ],
+  }) as [Q<Task>, Q<Task>, Q<Task>, Q<Task>];
+
+  if (
+    doneQ.isLoading ||
+    backlogQ.isLoading ||
+    inProgressQ.isLoading ||
+    reviewQ.isLoading
+  )
+    return <p>Loading…</p>;
+
+  if (
+    doneQ.isError ||
+    backlogQ.isError ||
+    inProgressQ.isError ||
+    reviewQ.isError
+  )
+    return <p>Error loading data</p>;
+
+  const dataByColumn: Record<string, Task[]> = {
+    backlog: backlogQ.data ?? [],
+    "in-progress": inProgressQ.data ?? [],
+    review: reviewQ.data ?? [],
+    done: doneQ.data ?? [],
+  };
   return (
     <div className=" bg-white p-4 d-flex  flex-column gap-3 rounded-3 ">
       {/* Search Bar */}
@@ -37,7 +94,7 @@ const HomePage = () => {
           >
             <KanbanColumn
               column={column}
-              tasks={[]}
+              tasks={dataByColumn[column.id]}
               onEdit={() => {}}
               onDelete={() => {}}
               searchTerm=""
