45e53eb082a97efcf1a01488a5207acad394531a
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2025-08-31 15:00:29 +0300

    feat: implement task fetching functions for review, backlog, in-progress, and done columns using axios

--- src/app/to-do/api/toDoApi.ts
+++ src/app/to-do/api/toDoApi.ts
@@ -1,7 +1,56 @@
+import axios from "axios";
+
 // NOTE: Temporary endpoint for testing/demo purposes only.
 const baseURL = "https://localhost:4000/tasks";
-export const fetchTasks = async () => {
-  const response = await fetch(baseURL);
-  const data = await response.json();
-  return data;
+export const fetchReviewTasks = async ({
+  limit = 5,
+  page = 1,
+
+  params = {},
+}) => {
+  const response = await axios.get(
+    `${baseURL}?column=review&_limit=${limit}&_page=${page}`,
+    {
+      params,
+    }
+  );
+  return response;
+};
+
+export const fetchBacklogTasks = async ({
+  limit = 5,
+  page = 1,
+  params = {},
+}) => {
+  const response = await axios.get(
+    `${baseURL}?column=backlog&_limit=${limit}&_page=${page}`,
+    {
+      params,
+    }
+  );
+  return response;
+};
+
+export const fetchInProgressTasks = async ({
+  limit = 5,
+  page = 1,
+  params = {},
+}) => {
+  const response = await axios.get(
+    `${baseURL}?column=in-progress&_limit=${limit}&_page=${page}`,
+    {
+      params,
+    }
+  );
+  return response;
+};
+
+export const fetchDoneTasks = async ({ limit = 5, page = 1, params = {} }) => {
+  const response = await axios.get(
+    `${baseURL}?column=done&_limit=${limit}&_page=${page}`,
+    {
+      params,
+    }
+  );
+  return response;
 };
