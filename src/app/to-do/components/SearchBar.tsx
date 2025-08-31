845a5a46201065f58ed56d846b6c727de5eebd91
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2025-08-31 13:01:50 +0300

    feat:  improved searchBar styling

--- src/app/to-do/components/SearchBar.tsx
+++ src/app/to-do/components/SearchBar.tsx
@@ -10,8 +10,8 @@ const SearchBar = ({
   setSearchTerm: (value: string) => void;
 }) => {
   return (
-    <div className=" w-50">
-      <div className="position-relative">
+    <div className=" col-12 col-md-7 col-lg-9   ">
+      <div className="position-relative w-100">
         {/* Icon inside the input */}
         <span
           className="position-absolute top-50 start-0 translate-middle-y ps-3 text-muted"
@@ -22,7 +22,7 @@ const SearchBar = ({
 
         <input
           type="text"
-          className="form-control ps-5 rounded-3"
+          className="form-control ps-5 rounded-3 w-100"
           placeholder="Search by task title or description..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
