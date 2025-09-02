6f461535e128cccb627f70099b521b7be73f2f5b
AG-Jimmy <ahmedgamalzenhom@gmail.com>
2025-09-02 14:56:40 +0300

    style: update layout and homepage styling for improved visual consistency and user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ReactQueryProvider>
-          <div className="">{children}</div>
+          <div className="  bg-black ">{children}</div>
         </ReactQueryProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,47 +5,43 @@ import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <>
-      <center className="min-vh-100 bg-primary ">
-        <div className=" d-flex flex-column justify-content-center  min-vh-100 container  gap-5 min-vh-50">
-          <div className="col-lg-6  ">
-            <h1 className="display-4 fw-bold mb-4">
-              Front-end Developer Assessment Task{" "}
-              <span className="text-warning d-block">Kanban To Do List</span>
-            </h1>
-            <p className="lead mb-4 text-white">
-              Streamline your project management with our intuitive Kanban-style
-              to-do list. Track progress, manage tasks, and boost productivity.
-            </p>
-            <div className="d-flex  justify-content-center gap-3">
-              <Link
-                href={"/to-do"}
-                className="btn btn-warning btn-lg d-flex align-items-center gap-2 fw-bold"
-              >
-                Get Started
-                <ArrowRight size={20} />
-              </Link>
-              <Link
-                href="https://github.com/AG-Jimmy/to-do-task"
-                className="btn btn-outline-light btn-lg"
-                target="_blank"
-              >
-                <Github />
-                Git hub
-              </Link>
-            </div>
-          </div>
-          <div className="col-lg-6 ">
-            <div className="text-center">
-              <div className="bg-white bg-opacity-10 rounded-3 p-4">
-                <CheckSquare size={120} className="text-warning mb-3" />
-                <h4 className="text-white">Ready to get organized?</h4>
-              </div>
-            </div>
+    <center className="min-vh-100 d-flex flex-column justify-content-center align-items-center  gap-5 ">
+      <div className="col-lg-6  ">
+        <h1 className="display-4 fw-bold mb-4">
+          Front-end Developer Assessment Task{" "}
+          <span className="text-warning d-block">Kanban To Do List</span>
+        </h1>
+        <p className="lead mb-4 text-white">
+          Streamline your project management with our intuitive Kanban-style
+          to-do list. Track progress, manage tasks, and boost productivity.
+        </p>
+        <div className="d-flex  justify-content-center gap-3">
+          <Link
+            href={"/to-do"}
+            className="btn btn-warning btn-lg d-flex align-items-center gap-2 fw-bold"
+          >
+            Get Started
+            <ArrowRight size={20} />
+          </Link>
+          <Link
+            href="https://github.com/AG-Jimmy/to-do-task"
+            className="btn btn-outline-light btn-lg"
+            target="_blank"
+          >
+            <Github />
+            Git hub
+          </Link>
+        </div>
+      </div>
+      <div className="col-lg-6 ">
+        <div className="text-center">
+          <div className="bg-white bg-opacity-10 rounded-3 p-4">
+            <CheckSquare size={120} className="text-warning mb-3" />
+            <h4 className="text-white">Ready to get organized?</h4>
           </div>
         </div>
-      </center>
-    </>
+      </div>
+    </center>
   );
 };
 
